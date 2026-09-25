2198dfa038ded9902e377a66a87b0b745a6aaa92
basuramita2024-coder <basuramita2024@gmail.com>
2026-09-25 17:12:58 +0530

    Add files via upload

---
+++ Sum of the following series.cpp
@@ -0,0 +1,26 @@
+//Write a C program to find the sum of the following series 1!+3!+5!+.....upto n number//
+//1! = 1
+//3! = 3*2*1 = 6
+//5! = 5*4*3*2*1 = 120
+#include <stdio.h>
+int main()
+{
+	int n;
+	int sum=0;
+	scanf("%d", &n);
+	int i=1;
+	while(i<=n)
+	{
+		int fact=1;
+		int j=1;
+		while(j<=i)
+		{
+			fact=fact*j;
+			j++;
+		}
+		sum+=fact;
+		i=i+2;
+	}   
+	printf("%d",sum);
+	return 0;
+}
