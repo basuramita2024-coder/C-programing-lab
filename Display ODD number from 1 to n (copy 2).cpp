855fceacc8c131e3f56819b408c5cf3f5a241ab6
basuramita2024-coder <basuramita2024@gmail.com>
2026-09-25 16:34:53 +0530

    Add files via upload

---
+++ Display ODD number from 1 to n (copy 2).cpp
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+int main()
+{
+	int n, i = 1;
+	
+	printf("Enter n: ");
+	scanf("%d", &n);
+	printf("Odd numbers from 1 to %d\n", n);
+	while(i <= n)
+	{
+		printf("%d\n",i);
+		i=i+2;
+	}
+}
