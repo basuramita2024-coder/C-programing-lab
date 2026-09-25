f0fec42c5f74353ecaa5a7287628b3207f845c81
basuramita2024-coder <basuramita2024@gmail.com>
2026-09-25 16:31:46 +0530

    Add files via upload

---
+++ Display ODD number from 1 to n.cpp
@@ -0,0 +1,19 @@
+/*Write a C program to display odd number from 1 to n*/
+#include <stdio.h>
+int main()
+{
+	int n, i = 1;
+	
+	printf("Enter n: ");
+	scanf("%d", &n);
+	
+	while(i <= n)
+	{
+		if(i % 2 != 0)
+		{
+			printf("%d", i);
+		}
+		i++;
+	}
+	return 0;
+} 
